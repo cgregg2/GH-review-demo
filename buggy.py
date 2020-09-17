3a7d0f29a30547a5eafed251b482b0cde4995cb0
Heather MacIsaac <hmacisaa@uwo.ca>
2020-09-17 14:04:16 -0400

    Bug fixes

--- buggy.py
+++ buggy.py
@@ -4,7 +4,7 @@ def calculate_bmi(weight, height):
     return weight / (height ** 2)
 
 for patient in patients:
-    weight, height = patients[0]
-    bmi = calculate_bmi(height, weight)
+    weight, height = patient
+    bmi = calculate_bmi(weight, height)
     print("Patient's BMI is: %f" % bmi)
 
