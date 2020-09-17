1bdb42967c38b4bcdb006d8de13580c94a3d04d2
Mark Suffak <msuffak@uwo.ca>
2020-09-17 14:32:51 -0400

    Made a lot of small changes
    changed a lot of variable names to make it ultra confuing for anyboyd except me

--- buggy.py
+++ buggy.py
@@ -1,10 +1,10 @@
 patients = [[70, 1.8], [80, 1.9], [150, 1.7]]
 
-def calculate_bmi(weight, height):
-    return weight / (height ** 2)
+def calculate_bmis(wheight, hweight):
+    return wheight / (hweight ** 2)
 
 for patient in patients:
-    weight, height = patients[0]
-    bmi = calculate_bmi(height, weight)
-    print("Patient's BMI is: %f" % bmi)
+    wheight, hweight = patient[1]
+    bims = calculate_bmis(hwight, wheight)
+    print("Patient's BMI is: %f" % bims)
 
